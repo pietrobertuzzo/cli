d0db8fb1654eaebe74c872421e8d706009ffae33
Pietro Bertuzzo <pietrobertuzzo@gmail.com>
2021-05-15 14:07:14 -0300

    header files created

---
+++ include/winbond_spi.h
@@ -0,0 +1,7 @@
+/*! @file winbond_spi.h
+*   @brief Definitions for Winbond SPI flash memory interface.
+*/
+
+#ifndef _WINBOND_SPI_H_
+#define _WINBOND_SPI_H_
+
---
+++ src/winbond_spi.c
@@ -0,0 +1,13 @@
+/*
+*   Created by Pietro Bertuzzo
+*   2021-05-15
+*
+*
+*/
+
+/*! @file winbond_spi.c
+*   @brief Interface for Winbond SPI flash memory.
+*/
+
+#include "winbond_spi.c"
+
